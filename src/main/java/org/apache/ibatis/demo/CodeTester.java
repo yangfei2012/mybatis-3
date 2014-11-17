bf5aac24ef6ac175b29b25e728b95e52c44968e5
fei <DaturaCheng@gmail.com>
2014-11-17 15:57:02 +0800

    非xml方式测试成功

--- src/main/java/org/apache/ibatis/demo/CodeTester.java
+++ src/main/java/org/apache/ibatis/demo/CodeTester.java
@@ -1,7 +1,9 @@
 package org.apache.ibatis.demo;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.TransactionFactory;
@@ -13,13 +15,30 @@ public class CodeTester {
 
     public static void main(String[] args) {
 
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/testdb");
+        dataSource.setDatabaseName("testdb");
+        dataSource.setUser("root");
+        dataSource.setPassword("1");
 
-//        DataSource dataSource = BlogDataSourceFactory.getBlogDataSource();
-//        TransactionFactory transactionFactory = new JdbcTransactionFactory();
-//        Environment environment = new Environment("development", transactionFactory, dataSource);
-//        Configuration configuration = new Configuration(environment);
-//        configuration.addMapper(UserMapper.class);
-//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+        TransactionFactory transactionFactory = new JdbcTransactionFactory();
+        Environment environment = new Environment("development", transactionFactory, dataSource);
+        Configuration configuration = new Configuration(environment);
+        configuration.addMapper(UserMapper.class);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            UserMapper mapper = session.getMapper(UserMapper.class);
+            User u = mapper.select(3);
+            System.out.println(u);
+
+            User u1 = mapper.selectByName("yangfei");
+            System.out.println(u1);
+
+        } finally {
+            session.close();
+        }
 
         System.out.println("xxxxxxxxxxxxx");
     }
